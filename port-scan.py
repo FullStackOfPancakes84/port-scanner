ce1f827890c54a50286bb05df7cb87329e314c7b
UkraineInTheMembrane <jason@ambitionenterprise.ca>
2019-11-25 00:43:06 -0800

    moved functions inside class

--- port-scan.py
+++ port-scan.py
@@ -1,36 +1,39 @@
 #!/usr/bin/env python3
 
 import socket 
-
-def install(package):
-    print('Installing %s' % package)
-    subprocess.call([sys.executable, '-m', 'pip', 'install', package])
+import subprocess
 
 try:
     from cprint import * 
 except:
-    import subprocess
     import sys 
-    install('cprint')
+    subprocess.call([sys.executable, '-m', 'pip', 'install', 'cprint'])
 
+# Set our variables
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 HOST = input('[+] Enter target IP: ')
-
 DEFAULT_TIMEOUT = 0.5
 
-def scan_port(port, timeout=DEFAULT_TIMEOUT):
-    s = socket.socket()
-    s.settimeout(timeout)
-    try: 
-        s.connect((HOST, port))
-        return True
-    except:
-        return False
+class PortScanner:
+    def __init__(self, port, address):
+        self.port = port 
+        self.address = address
 
-for i in range(1,1024):
-    if scan_port(i):
-        cprint.info('%d [+] connection successful' % (i))
+    def scan_port(self, port):
+        s = socket.socket()
+        try:
+            s.connect((HOST, port))
+            return True
+        except:
+            return False
+
+for i in range(444):
+    scanner = PortScanner(i, HOST)
+    if scanner.scan_port(i):
+        cprint.info('%d [+] connection successful' % (i) )
         s.close()
+
+
+
 
     
